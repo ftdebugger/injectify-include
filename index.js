ff500db69d6b63ac071548294a4f46934f7e4b7c
Evgeny Shpilevsky <evgeny@shpilevsky.com>
2014-07-22 00:09:50 +0300

    Support mixin hash and options.hash for support
    {{include "some-tpl" this}}

--- index.js
+++ index.js
@@ -7,8 +7,37 @@
      */
     var Handlebars = require("injectify/runtime");
 
-    Handlebars.registerHelper("include", function (template, options) {
-        return new Handlebars.SafeString(template(options.hash));
-    });
+    /**
+     * @param {Function} template
+     * @param {Object} [hash]
+     * @param {{hash: Object}} options
+     *
+     * @returns {Handlebars.SafeString}
+     */
+    var includeHelper = function () {
+        var args = _.toArray(arguments),
+            options = args.pop(),
+            template = args.shift(),
+            hash;
+
+        if (args.length) {
+            hash = args.pop();
+        }
+
+        if (hash) {
+            hash = _.extend({}, hash, options.hash);
+        }
+        else {
+            hash = options.hash;
+        }
+
+        hash.__parent__ = this;
+
+        return new Handlebars.SafeString(template(hash));
+    };
+
+    Handlebars.registerHelper("include", includeHelper);
+
+    module.exports = includeHelper;
 
 })();
